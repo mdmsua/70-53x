d9dcb75419732434b9c9fcfa78834e9c4810d60f
Dmytro Morozov <dmm@timemsystem.com>
2014-11-11 00:13:50 +0200

    Initial commit with SSO 7053x.onmicrosoft.com

---
+++ 70-53x/WebApplication/Utils/DatabaseIssuerNameRegistry.cs
@@ -0,0 +1,90 @@
+﻿using System.IdentityModel.Tokens;
+using System.Linq;
+using WebApplication.Models;
+
+namespace WebApplication.Utils
+{
+    public class DatabaseIssuerNameRegistry : ValidatingIssuerNameRegistry
+    {
+        public static bool ContainsTenant(string tenantId)
+        {
+            using (TenantDbContext context = new TenantDbContext())
+            {
+                return context.Tenants.Any(tenant => tenant.Id == tenantId);
+            }
+        }
+
+        public static bool ContainsKey(string thumbprint)
+        {
+            using (TenantDbContext context = new TenantDbContext())
+            {
+                return context.IssuingAuthorityKeys.Any(key => key.Id == thumbprint);
+            }
+        }
+
+        public static void RefreshKeys(string metadataLocation)
+        {
+            IssuingAuthority issuingAuthority = GetIssuingAuthority(metadataLocation);
+
+            bool newKeys = false;
+            bool refreshTenant = false;
+            foreach (string thumbprint in issuingAuthority.Thumbprints)
+            {
+                if (!ContainsKey(thumbprint))
+                {
+                    newKeys = true;
+                    refreshTenant = true;
+                    break;
+                }
+            }
+
+            foreach (string issuer in issuingAuthority.Issuers)
+            {
+                if (!ContainsTenant(GetIssuerId(issuer)))
+                {
+                    refreshTenant = true;
+                    break;
+                }
+            }
+
+            if (newKeys || refreshTenant)
+            {
+                using (TenantDbContext context = new TenantDbContext())
+                {
+                    if (newKeys)
+                    {
+                      context.IssuingAuthorityKeys.RemoveRange(context.IssuingAuthorityKeys);
+                      foreach (string thumbprint in issuingAuthority.Thumbprints)
+                      {
+                          context.IssuingAuthorityKeys.Add(new IssuingAuthorityKey { Id = thumbprint });
+                      }
+                    }
+
+                    if (refreshTenant)
+                    {
+                        foreach (string issuer in issuingAuthority.Issuers)
+                        {
+                            string issuerId = GetIssuerId(issuer);
+                            if (!ContainsTenant(issuerId))
+                            {
+                                context.Tenants.Add(new Tenant { Id = issuerId });
+                            }
+                        }
+                    }
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        private static string GetIssuerId(string issuer)
+        {
+            return issuer.TrimEnd('/').Split('/').Last();
+        }
+
+        protected override bool IsThumbprintValid(string thumbprint, string issuer)
+        {
+            return ContainsTenant(GetIssuerId(issuer))
+                && ContainsKey(thumbprint);
+        }
+    }
+}
