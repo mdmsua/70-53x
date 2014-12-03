5d6e7b3a6ce015725692cdc5e746b523003fc9c0
Dmytro Morozov <dmm@timemsystem.com>
2014-12-03 17:08:05 +0200

    Revert "Custom claims authentication manager"
    This reverts commit 27a10f653e2c231d4df887c4f5229bc1a826895a.

--- 70-53x/WebApplication/Controllers/HomeController.cs
+++ 70-53x/WebApplication/Controllers/HomeController.cs
@@ -1,58 +1,26 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="HomeController.cs" company="70-53x">
-//   70-53x
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
+﻿using System.Web.Mvc;
 
 namespace WebApplication.Controllers
 {
-    using System.Web.Mvc;
-
-    /// <summary>
-    /// The home controller.
-    /// </summary>
     public class HomeController : Controller
     {
-        #region Public Methods and Operators
-
-        /// <summary>
-        /// The about.
-        /// </summary>
-        /// <returns>
-        /// The <see cref="ActionResult"/>.
-        /// </returns>
-        public ActionResult About()
-        {
-            this.ViewBag.Message = "Your application description page.";
-
-            return this.View();
-        }
-
-        /// <summary>
-        /// The contact.
-        /// </summary>
-        /// <returns>
-        /// The <see cref="ActionResult"/>.
-        /// </returns>
-        [Authorize(Roles = "Company Administrator, User Account Administrator")]
-        public ActionResult Contact()
-        {
-            this.ViewBag.Message = "Your contact page.";
-
-            return this.View();
-        }
-
-        /// <summary>
-        /// The index.
-        /// </summary>
-        /// <returns>
-        /// The <see cref="ActionResult"/>.
-        /// </returns>
         public ActionResult Index()
         {
-            return this.View();
+            return View();
         }
 
-        #endregion
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
